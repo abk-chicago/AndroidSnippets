4b9bf0e9cbccceedb77de198b39b0a22ebe10b4b
James <code-for-coffee@users.noreply.github.com>
2016-03-30 01:27:01 -0500

    adding a generic location file that expects some json

---
+++ GenericLocationModel.java
@@ -0,0 +1,147 @@
+public class GenericLocationModel {
+
+    LocationModel(JSONObject Json) throws JSONException, MalformedURLException {
+
+        for(int i = 0; i< Json.names().length(); i++){
+            String key = Json.names().getString(i);
+            String val =  Json.get(Json.names().getString(i)).toString();
+
+            switch (val) {
+                case "city":
+                    this.City = val;
+                    break;
+                case "name":
+                    this.Name = val;
+                    break;
+                case "website_url":
+                    this.Url = new URL(val);
+                    break;
+                case "id":
+                    this.Id = new Integer(val);
+                    break;
+                case "address":
+                    this.Address = val;
+                    break;
+                case "state":
+                    this.State = val;
+                    break;
+                case "zipcode":
+                    this.ZipCode = val;
+                    break;
+                case "latitude":
+                    this.Latitude = new Double(val);
+                    break;
+                case "longitude":
+                    this.Longitude = new Double(val);
+                    break;
+                case "photo":
+                    this.Photo = new URL(val);
+                    break;
+                case "email":
+                    this.Email = val;
+                    break;
+            }
+        }
+    }
+
+    // public information
+    public Integer Id;
+    public String City;
+    public String Name;
+    public URL Url;
+    public String Address;
+    public String State;
+    public String ZipCode;
+    public Double Latitude;
+    public Double Longitude;
+    public URL Photo;
+    public String Email;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public URL getUrl() {
+        return Url;
+    }
+
+    public void setUrl(URL url) {
+        Url = url;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public String getZipCode() {
+        return ZipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        ZipCode = zipCode;
+    }
+
+    public Double getLatitude() {
+        return Latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        Latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        Longitude = longitude;
+    }
+
+    public URL getPhoto() {
+        return Photo;
+    }
+
+    public void setPhoto(URL photo) {
+        Photo = photo;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+}
